1a8f1c614f74ec1d11b9fd53883271f988947e1d
FourDizzle <ndcassiani@gmail.com>
2016-05-30 22:48:15 -0500

    Redid architechure (a lot) applied google java style

--- src/main/java/us/superkill/mean_machine/detect/FaceDetector.java
+++
@@ -1,9 +0,0 @@
-package us.superkill.mean_machine.detect;
-
-import org.bytedeco.javacpp.opencv_core.Mat;
-import org.bytedeco.javacpp.opencv_core.Rect;
-
-public abstract class FaceDetector {
-	
-	public abstract Rect[] detectFaces(Mat image);
-}
